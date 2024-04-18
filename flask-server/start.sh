937cce2f57afd4843fcd8f568860b6f832e455cd
iffigueroa <isabella.figueroa@colorado.edu>
2024-04-18 12:03:29 -0700

    fix docker connection refused error

--- flask-server/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PORT=5000
-IMAGE_NAME='goalzilla-flask'
-
-docker build -t $IMAGE_NAME .
-docker run -p $PORT:$PORT $IMAGE_NAME
--- goalzilla-react/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PORT=3000
-IMAGE_NAME='goalzilla-react'
-
-docker build -t $IMAGE_NAME .
-docker run -p $PORT:$PORT $IMAGE_NAME
