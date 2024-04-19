37d3090c294bd2181c3f253142d9e107e742a7b7
iffigueroa <isabella.figueroa@colorado.edu>
2024-04-19 14:49:02 -0700

    clean up server & helpers

--- flask-server/request_config.py
+++ flask-server/request_config.py
@@ -1,14 +1,61 @@
 
-REQUIRED_VALUES = {
-    'journeyDetails': [('journeyIdx', int)], 
-    'quest_preview': [('journeyIdx', int)], 
-    'questDetails': [('journeyIdx', int), ('questIdx', int)], 
-    'getTaskDetails': [('journeyIdx', int), ('questIdx', int), ('taskIdx', int)], 
-    'remove_quest': [('journeyIdx', int), ('questIdx', int)], 
-    'add_goal': [('name', str), ('description', str)], 
-    'remove_journey': [('index', int)], 
-    'add_quest': [('journeyIdx', int), ('name', str), ('description', str)], 
-    'remove_task': [('journeyIdx', int), ('questIdx', int), ('taskIdx', int)], 
-    'add_task': [('journeyIdx', int), ('questIdx', int), ('taskName', str)], 
-    'add_task_completion': [('journeyIdx', int), ('questIdx', int), ('taskIdx', int)], 
+
+ROUTE_INFO = {
+    'journeyDetails': {
+        'parameters': [('journeyIdx', int)],
+        'method': 'GET',
+        'handler': 'get_journey_details',
+    }, 
+    'quest_preview': {
+        'parameters': [('journeyIdx', int)],
+        'method': 'GET',
+        'handler': 'get_quest_preview',
+    },
+
+    'questDetails':  {
+        'parameters':[('journeyIdx', int), ('questIdx', int)], 
+        'method': 'GET',
+        'handler': 'get_quest_details',
+    },
+
+    'getTaskDetails':  {
+        'parameters':[('journeyIdx', int), ('questIdx', int), ('taskIdx', int)], 
+        'method': 'GET',
+        'handler': 'get_task_details',
+    },
+    'remove_quest':  {
+        'parameters': [('journeyIdx', int), ('questIdx', int)], 
+        'method': 'POST',
+        'handler': 'remove_quest',
+    },
+    'add_goal':  {
+        'parameters': [('name', str), ('description', str)], 
+        'method': 'POST',
+        'handler': 'add_journey',
+    },
+    'remove_journey':  {
+        'parameters': [('index', int)], 
+        'method': 'POST',
+        'handler': 'remove_journey',
+    },
+    'add_quest':  {
+        'parameters': [('journeyIdx', int), ('name', str), ('description', str)], 
+        'method': 'POST',
+        'handler': 'add_quest',
+    },
+    'remove_task':  {
+        'parameters': [('journeyIdx', int), ('questIdx', int), ('taskIdx', int)], 
+        'method': 'POST',
+        'handler': 'remove_task',
+    },
+    'add_task':  {
+        'parameters': [('journeyIdx', int), ('questIdx', int), ('taskName', str)], 
+        'method': 'POST',
+        'handler': 'add_task',
+    },
+    'add_task_completion':  {
+        'parameters': [('journeyIdx', int), ('questIdx', int), ('taskIdx', int)],
+        'method': 'POST',
+        'handler': 'complete_task',
+    },
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,49 @@
 from flask import Flask, request, jsonify
+from request_config import ROUTE_INFO
 from GoalzillaData import GoalzillaData
-from helper import extract_args
 
 app = Flask(__name__)
-
 app_data = GoalzillaData(include_test_defaults=True)
+
+""" Route Handling Helpers"""
+
+def extract_args(request, route: str, route_details: dict):
+    if route_details['method']=="POST":
+        params = request.get_json()
+    else:
+        params = request.args
+    values_needed = route_details['parameters']
+    if not values_needed: 
+        raise Exception(f"No required values found for {route}.")
+    kwargs = {}
+    for value, value_type in values_needed: 
+        arg = params.get(value)
+        if arg == None: 
+            raise Exception(f"Value for {value} is missing.") 
+        try:
+            casted_value = value_type(arg)
+        except ValueError:
+            raise Exception(f"Failed to cast value {arg} to type {value_type} for {value}.")
+        kwargs[value] = casted_value
+    print(kwargs)
+    return kwargs
+
+def handle_request(request, route):
+    route_details = ROUTE_INFO.get(route)
+    if not route_details:
+        raise Exception(f"NO ROUTE FOUND: {route}")
+    
+    handler = getattr(app_data, route_details['handler'])
+
+    if (kwargs := extract_args(request, route, route_details)):
+        try: 
+            result = handler(**kwargs)
+            if result:
+                return result
+        except Exception as e:
+            return jsonify({"message": f"Failed to execute request {route} w/exception {e}"})
+    return jsonify({'message': f'Successful execution of {request}'})
+
 
 @app.route("/goals")
 def get_goals():
@@ -12,71 +51,48 @@ def get_goals():
 
 @app.route("/add_goal", methods=['POST'])
 def add_goal():
-    if (kwargs := extract_args(request, 'add_goal', post=True)):
-        app_data.add_journey(**kwargs)
-    return jsonify({'message': f'Goal added successfully'})
+    return handle_request(request, 'add_goal')
 
 @app.route("/remove_journey", methods=['POST'])
 def remove_journey():
-    if (kwargs := extract_args(request, 'remove_journey', post=True)):
-        app_data.remove_journey(**kwargs)
-    return jsonify({'message': f'Goal removed successfully'})
+    return handle_request(request, 'remove_journey')
 
 @app.route("/quest_preview")
 def get_quest_preview():
-    if (kwargs := extract_args(request, 'quest_preview')):
-        return app_data.get_quest_preview(**kwargs)
+    return handle_request(request, 'quest_preview')
 
 
 @app.route("/remove_quest", methods=['POST'])
 def remove_quest():
-    if (kwargs := extract_args(request, 'remove_quest', post=True)):
-        app_data.remove_quest(**kwargs)
-    return jsonify({'message': 'Quest removed.'})
-
+    return handle_request(request, 'remove_quest')
 
 @app.route("/add_quest", methods=['POST'])
 def add_quest():
-    if (kwargs := extract_args(request, 'add_quest', post=True)):
-        app_data.add_quest(**kwargs)
-    return jsonify({'message': 'Quest added.'})
+    return handle_request(request, 'add_quest')
 
 @app.route("/journeyDetails")
 def getJourneyDetails():
-    if (kwargs := extract_args(request, 'journeyDetails')):
-        return app_data.get_journey_details(**kwargs)
-    return {'error': 'No journey with provided id.'}
+    return handle_request(request, 'journeyDetails')
 
 @app.route('/questDetails')
 def get_quest_details():
-    if (kwargs := extract_args(request, 'questDetails')):
-        return app_data.get_quest_details(**kwargs)
-    return {'error': 'No journey with provided id.'}
+    return handle_request(request, 'questDetails')
 
 @app.route('/getTaskDetails')
 def get_task_details():
-    if (kwargs := extract_args(request, 'getTaskDetails')):
-        return app_data.get_task_details(**kwargs)
-    return {'error': 'No journey with provided id.'}
+    return handle_request(request, 'getTaskDetails')
 
 @app.route("/remove_task", methods=['POST'])
 def remove_task():
-    if (kwargs := extract_args(request, 'remove_task', post=True)):
-        app_data.remove_task(**kwargs)
-    return jsonify({'message': 'task removed.'})    
+    return handle_request(request, 'remove_task')
 
 @app.route("/add_task", methods=['POST'])
 def add_task():
-    if (kwargs := extract_args(request, 'add_task', post=True)):
-        app_data.add_task(**kwargs)
-    return jsonify({'message': 'Task added.'})
+    return handle_request(request, 'add_task')
 
 @app.route("/add_task_completion", methods=['POST'])
 def add_task_completion():
-    if (kwargs := extract_args(request, 'add_task_completion', post=True)):
-        app_data.complete_task(**kwargs)
-    return jsonify({'message': 'Task Completed.'})
-    
+    return handle_request(request, 'add_task_completion')
 
 
 if __name__ == '__main__':
